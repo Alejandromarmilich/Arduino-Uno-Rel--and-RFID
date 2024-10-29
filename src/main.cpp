21d61a9a6f8e94e45c814e5b00044f395a24e999
Alejandro Herrera <33562302+Alejandromarmilich@users.noreply.github.com>
2024-10-29 16:29:06 -0300

    Servo sg90

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,24 @@
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>  // Librería para controlar el servo
 
 #define SS_PIN 10
 #define RST_PIN 9
 #define BUZZER_PIN 8    
 #define LED_ROJO_PIN 7  
 #define LED_AMARILLO_PIN 6  
-#define RELE_PIN 5      
+#define SERVO_PIN 5      // Pin del servo
 #define LED_AZUL_PIN 4 
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7);
+Servo servo;  // Crear un objeto Servo
 
 byte llavePermitida[] = {0xAB, 0x31, 0xA6, 0x1B}; 
 
 bool sistemaEncendido = false;
+unsigned long ultimoMovimiento = 0;
 
 void apagarSistema();
 void encenderSistema();
@@ -37,8 +40,10 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);  
   pinMode(LED_ROJO_PIN, OUTPUT);
   pinMode(LED_AMARILLO_PIN, OUTPUT);
-  pinMode(RELE_PIN, OUTPUT);
   pinMode(LED_AZUL_PIN, OUTPUT);
+
+  servo.attach(SERVO_PIN);  // Conectar el servo al pin especificado
+  servo.write(0);  // Inicialmente en posición 0°
 
   apagarSistema();
   
@@ -94,6 +99,12 @@ void loop() {
   }
 
   delay(1000);
+
+  if (sistemaEncendido && millis() - ultimoMovimiento >= 5000) {
+    int posicionAleatoria = random(0, 181);  // Generar posición aleatoria entre 0° y 180°
+    servo.write(posicionAleatoria);
+    ultimoMovimiento = millis();
+  }
 }
 
 bool compararLlave(byte* uidLeido) {
@@ -121,8 +132,8 @@ void apagarSistema() {
   sistemaEncendido = false;
   digitalWrite(LED_ROJO_PIN, HIGH);     
   digitalWrite(LED_AMARILLO_PIN, LOW);  
-  digitalWrite(RELE_PIN, LOW);          
   digitalWrite(LED_AZUL_PIN, LOW);
+  servo.write(0);  // Llevar el servo a 0°
   Serial.println("Sistema apagado.");
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -133,7 +144,6 @@ void encenderSistema() {
   sistemaEncendido = true;
   digitalWrite(LED_ROJO_PIN, LOW);      
   digitalWrite(LED_AMARILLO_PIN, HIGH); 
-  digitalWrite(RELE_PIN, HIGH);
   delay(500);    
   digitalWrite(LED_AZUL_PIN, HIGH);
   digitalWrite(LED_AMARILLO_PIN, LOW); 
